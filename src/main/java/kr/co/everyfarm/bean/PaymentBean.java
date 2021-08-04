b00e7c1141cc7c2fbd8e466cee2d74cff10d87a9
hj <rjarorgudwns@naver.com>
2021-08-04 21:27:01 +0900

    bean 패키지 및 bean 파일 생성

---
+++ src/main/java/kr/co/everyfarm/bean/PaymentBean.java
@@ -0,0 +1,150 @@
+package kr.co.everyfarm.bean;
+
+public class PaymentBean {
+	
+	private String pay_Id;
+	private String pay_Name;
+	private String pay_Address;
+	private String pay_Email;
+	private String pay_Nomemberpw;
+	private String pay_Title;
+	private String pay_Method;
+	private String pay_Sub;
+	private int pay_Tel;
+	private int pay_No;
+	private int pay_Orderno;
+	private int pay_Land;
+	private int pay_Landprice;
+	private int pay_Seedprice;
+	private int pay_Manpay;
+	private int pay_Totalprice;
+	private int pay_Methodno;
+	private int pay_Delivery;
+	private int pay_Admin;
+	
+	public String getPay_Id() {
+		return pay_Id;
+	}
+	public void setPay_Id(String pay_Id) {
+		this.pay_Id = pay_Id;
+	}
+	public String getPay_Name() {
+		return pay_Name;
+	}
+	public void setPay_Name(String pay_Name) {
+		this.pay_Name = pay_Name;
+	}
+	public String getPay_Address() {
+		return pay_Address;
+	}
+	public void setPay_Address(String pay_Address) {
+		this.pay_Address = pay_Address;
+	}
+	public String getPay_Email() {
+		return pay_Email;
+	}
+	public void setPay_Email(String pay_Email) {
+		this.pay_Email = pay_Email;
+	}
+	public String getPay_Nomemberpw() {
+		return pay_Nomemberpw;
+	}
+	public void setPay_Nomemberpw(String pay_Nomemberpw) {
+		this.pay_Nomemberpw = pay_Nomemberpw;
+	}
+	public String getPay_Title() {
+		return pay_Title;
+	}
+	public void setPay_Title(String pay_Title) {
+		this.pay_Title = pay_Title;
+	}
+	public String getPay_Method() {
+		return pay_Method;
+	}
+	public void setPay_Method(String pay_Method) {
+		this.pay_Method = pay_Method;
+	}
+	public String getPay_Sub() {
+		return pay_Sub;
+	}
+	public void setPay_Sub(String pay_Sub) {
+		this.pay_Sub = pay_Sub;
+	}
+	public int getPay_Tel() {
+		return pay_Tel;
+	}
+	public void setPay_Tel(int pay_Tel) {
+		this.pay_Tel = pay_Tel;
+	}
+	public int getPay_No() {
+		return pay_No;
+	}
+	public void setPay_No(int pay_No) {
+		this.pay_No = pay_No;
+	}
+	public int getPay_Orderno() {
+		return pay_Orderno;
+	}
+	public void setPay_Orderno(int pay_Orderno) {
+		this.pay_Orderno = pay_Orderno;
+	}
+	public int getPay_Land() {
+		return pay_Land;
+	}
+	public void setPay_Land(int pay_Land) {
+		this.pay_Land = pay_Land;
+	}
+	public int getPay_Landprice() {
+		return pay_Landprice;
+	}
+	public void setPay_Landprice(int pay_Landprice) {
+		this.pay_Landprice = pay_Landprice;
+	}
+	public int getPay_Seedprice() {
+		return pay_Seedprice;
+	}
+	public void setPay_Seedprice(int pay_Seedprice) {
+		this.pay_Seedprice = pay_Seedprice;
+	}
+	public int getPay_Manpay() {
+		return pay_Manpay;
+	}
+	public void setPay_Manpay(int pay_Manpay) {
+		this.pay_Manpay = pay_Manpay;
+	}
+	public int getPay_Totalprice() {
+		return pay_Totalprice;
+	}
+	public void setPay_Totalprice(int pay_Totalprice) {
+		this.pay_Totalprice = pay_Totalprice;
+	}
+	public int getPay_Methodno() {
+		return pay_Methodno;
+	}
+	public void setPay_Methodno(int pay_Methodno) {
+		this.pay_Methodno = pay_Methodno;
+	}
+	public int getPay_Delivery() {
+		return pay_Delivery;
+	}
+	public void setPay_Delivery(int pay_Delivery) {
+		this.pay_Delivery = pay_Delivery;
+	}
+	public int getPay_Admin() {
+		return pay_Admin;
+	}
+	public void setPay_Admin(int pay_Admin) {
+		this.pay_Admin = pay_Admin;
+	}
+	
+	@Override
+	public String toString() {
+		return "PaymentBean [pay_Id=" + pay_Id + ", pay_Name=" + pay_Name + ", pay_Address=" + pay_Address
+				+ ", pay_Email=" + pay_Email + ", pay_Nomemberpw=" + pay_Nomemberpw + ", pay_Title=" + pay_Title
+				+ ", pay_Method=" + pay_Method + ", pay_Sub=" + pay_Sub + ", pay_Tel=" + pay_Tel + ", pay_No=" + pay_No
+				+ ", pay_Orderno=" + pay_Orderno + ", pay_Land=" + pay_Land + ", pay_Landprice=" + pay_Landprice
+				+ ", pay_Seedprice=" + pay_Seedprice + ", pay_Manpay=" + pay_Manpay + ", pay_Totalprice="
+				+ pay_Totalprice + ", pay_Methodno=" + pay_Methodno + ", pay_Delivery=" + pay_Delivery + ", pay_Admin="
+				+ pay_Admin + "]";
+	}
+}
---
+++ src/main/java/kr/co/everyfarm/bean/QnABean.java
@@ -0,0 +1,5 @@
+package kr.co.everyfarm.bean;
+
+public class QnABean {
+
+}
